201860742d538c696fddc24a23594a5d9666bdb2
Matt Isherwood <matt@mishmedia.co.uk>
2022-12-10 23:07:50 +0000

    feat: make card urls & descriptions optional

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,17 +1,30 @@
 import Link from "next/link"
+import { ReactNode } from "react"
 import classes from "./Card.module.css"
 
 type Props = {
   title: string
-  description: string
-  url: string
+  description?: string
+  url?: string
 }
 
-export const Card = ({ title, description, url }: Props) => {
-  return (
-    <Link href={url} className={classes.card}>
-      <h2>{title} &rarr;</h2>
-      <p>{description}</p>
-    </Link>
-  )
+type WrapperProps = {
+  children: ReactNode
+  url?: string
 }
+
+export const Card = ({ title, description, url }: Props) => (
+  <CardWrapper url={url}>
+    <h2>{title}</h2>
+    {description && <p>{description}</p>}
+  </CardWrapper>
+)
+
+const CardWrapper = ({ children, url }: WrapperProps) =>
+  url ? (
+    <Link href={url} className={classes.card}>
+      {children}
+    </Link>
+  ) : (
+    <div className={classes.card}>{children}</div>
+  )
