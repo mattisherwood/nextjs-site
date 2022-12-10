795088bff906f8831cdbfaf179252ec876744d2b
Matt Isherwood <matt@mishmedia.co.uk>
2022-12-10 12:30:47 +0000

    fix: rename pages to tsx extension + update prop

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,13 @@
 import { Footer, Head, Header } from "../components"
 import styles from "../styles/Home.module.css"
 
-const title = "Contact"
+const title = "About"
 
-export default function Contact() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head title={title} />
-      <Header title={title} />
+      <Header currentPage={title} />
       <main className={styles.main}>
         <h1 className={styles.title}>This is the {title} Page!</h1>
       </main>
