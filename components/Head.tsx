0f8d4351a8d3a46cc5e05f5fd9933a4c3168ec2b
Matt Isherwood <matt@mishmedia.co.uk>
2022-12-10 23:22:46 +0000

    feat: improve page metatitles

--- components/Head.tsx
+++ components/Head.tsx
@@ -4,10 +4,15 @@ type Props = {
   title: string
 }
 
+const websiteTitle = "NextJS Website"
+
 export const Head = ({ title }: Props) => {
   return (
     <NextHead>
-      <title>{title}</title>
+      <title>
+        {title != "Home" ? `${title} | ` : null}
+        {websiteTitle}
+      </title>
       <meta name='description' content='Using React/Typescript' />
       <link rel='icon' href='/favicon.ico' />
     </NextHead>
