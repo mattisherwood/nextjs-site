170d1c1ec6d5cffbcf386206e12a18475380b583
Matt Isherwood <matt@mishmedia.co.uk>
2022-12-23 00:31:42 +0000

    fix: update link story

--- components/Link/Link.stories.tsx
+++ components/Link/Link.stories.tsx
@@ -1,10 +1,23 @@
-import { Link } from './Link'
+import { Link } from "./Link"
 
 export const Base = () => (
-  <Link title="Link Component" />
+  <ul>
+    <li>
+      <Link href='about'>Relative Link</Link>
+    </li>
+    <li>
+      <Link href='/about'>Root Relative Link</Link>
+    </li>
+    <li>
+      <Link href='https://www.google.com'>External Link</Link>
+    </li>
+    <li>
+      <Link href='#section'>Anchor Link</Link>
+    </li>
+  </ul>
 )
 
 export default {
   component: Link,
-  title: 'atoms/Link',
+  title: "atoms/Link",
 }
